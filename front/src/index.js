380e21f6d41b7665077cb4257814c73684992056
Variandr <maxim.savchenko.job@gmail.com>
2022-04-17 13:42:26 +0300

    Fixed CORS policy

--- front/src/index.js
+++ front/src/index.js
@@ -4,7 +4,7 @@ import './index.css'
 import store from './state/store'
 import App from './App'
 import {Provider} from "react-redux"
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom"
 
 ReactDOM.render(
     <React.StrictMode>
